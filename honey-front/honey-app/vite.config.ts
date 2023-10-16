9e5d4779e2d8a721016b20bb0caa9edc731b4688
YS <dbtjq1592000@gmail.com>
2023-10-16 14:14:25 +0900

    :tada: init : 꿀스라이팅 초기 설정

---
+++ honey-front/honey-app/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port : 3000,
+  },
+  resolve: {
+    alias: [
+      {find: "@", replacement: path.resolve(__dirname, "src")},
+      {
+        find: "@assets",
+        replacement: path.resolve(__dirname, "src/assets"),
+      },
+
+    ]
+  }
+})
