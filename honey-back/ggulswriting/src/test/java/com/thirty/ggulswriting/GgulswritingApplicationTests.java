ac4fdd49f6936c96771e0712a95117bb0de7d860
김동현 <dhkim960511@naver.com>
2023-10-18 16:08:35 +0900

    :tada: init: create honey-backend

---
+++ honey-back/ggulswriting/src/test/java/com/thirty/ggulswriting/GgulswritingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirty.ggulswriting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgulswritingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
