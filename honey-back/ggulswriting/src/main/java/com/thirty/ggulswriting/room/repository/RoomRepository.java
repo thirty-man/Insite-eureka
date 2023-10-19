4b82b4781f9d470db0ae0e58d7a0f436ed55ca80
박현철 <wns882@naver.com>
2023-10-19 11:15:45 +0900

    :zap: feat: 방 검색 쿼리 추가

---
+++ honey-back/ggulswriting/src/main/java/com/thirty/ggulswriting/room/repository/RoomRepository.java
@@ -0,0 +1,14 @@
+package com.thirty.ggulswriting.room.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.thirty.ggulswriting.member.entity.Member;
+import com.thirty.ggulswriting.room.entity.Room;
+
+public interface RoomRepository extends JpaRepository<Room, Integer> {
+	Optional<Room> findRoomByRoomId(Long roomId);
+
+	Optional<Room> findRoomByMember(Member member);
+}
