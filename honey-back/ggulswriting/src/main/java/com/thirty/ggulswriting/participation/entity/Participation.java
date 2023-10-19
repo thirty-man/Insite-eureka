53c40be67f2c2749e590be74c747e48c43991560
박현철 <wns882@naver.com>
2023-10-19 11:13:44 +0900

    :zap: feat: 정적팩터리메서드 추가

--- honey-back/ggulswriting/src/main/java/com/thirty/ggulswriting/participation/entity/Participation.java
+++ honey-back/ggulswriting/src/main/java/com/thirty/ggulswriting/participation/entity/Participation.java
@@ -1,8 +1,5 @@
 package com.thirty.ggulswriting.participation.entity;
 
-import com.thirty.ggulswriting.global.entity.BaseEntity;
-import com.thirty.ggulswriting.member.entity.Member;
-import com.thirty.ggulswriting.room.entity.Room;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +8,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.thirty.ggulswriting.global.entity.BaseEntity;
+import com.thirty.ggulswriting.member.entity.Member;
+import com.thirty.ggulswriting.room.entity.Room;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,26 +27,26 @@ import lombok.NoArgsConstructor;
 @Table(name = "Participation")
 public class Participation extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long participationId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long participationId;
 
-    @ManyToOne
-    @JoinColumn(name = "member_id",nullable = false)
-    private Member member;
+	@ManyToOne
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
 
-    @ManyToOne
-    @JoinColumn(name = "room_id",nullable = false)
-    private Room room;
+	@ManyToOne
+	@JoinColumn(name = "room_id", nullable = false)
+	private Room room;
 
-    @Column(nullable = false)
-    private Boolean isOut;
+	@Column(nullable = false)
+	private Boolean isOut;
 
-    public Participation create(Member member, Room room, Boolean isOut){
-        return Participation.builder()
-            .member(member)
-            .room(room)
-            .isOut(isOut)
-            .build();
-    }
+	public static Participation of(Member member, Room room, Boolean isOut) {
+		return Participation.builder()
+			.member(member)
+			.room(room)
+			.isOut(isOut)
+			.build();
+	}
 }
