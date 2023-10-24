c617131cadbf0d778ce971129a20bb486722c293
김동현 <dhkim960511@naver.com>
2023-10-24 11:22:27 +0900

    :sparkles: feat: add password filtering

--- honey-back/ggulswriting/src/main/java/com/thirty/ggulswriting/global/error/ErrorCode.java
+++ honey-back/ggulswriting/src/main/java/com/thirty/ggulswriting/global/error/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
 	//room
 	NOT_EXIST_ROOM(HttpStatus.BAD_REQUEST, "001", "존재하지 않는 방입니다."),
 	DELETED_ROOM(HttpStatus.BAD_REQUEST, "002", "삭제된 방입니다."),
+	NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "003", "방 비밀번호가 일치하지 않습니다"),
 	//참가자
 	ALREADY_EXIST_MEMBER(HttpStatus.BAD_REQUEST, "001", "이미 방에 참가중인 회원입니다."),
 	NOT_EXIST_PARTICIPATION(HttpStatus.BAD_REQUEST, "002", "방에 참여하지 않은 회원입니다."),
