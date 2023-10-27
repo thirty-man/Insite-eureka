96cef12fd8c0c86b5a9f65811dedef0ec7ea23ab
YS <dbtjq1592000@gmail.com>
2023-10-27 11:12:38 +0900

    :hammer: fix: axios config 추가

--- honey-front/honey-app/src/main.tsx
+++ honey-front/honey-app/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import App from "./App";
 import "@styles/global.css";
+import "@config/axios.config";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
