312cfede3d0459bf20ca4f4a7a292e7b49e0bdfb
wns882@naver.com <wns882@naver.com>
2023-10-28 12:47:27 +0900

    :tada: init: APIGATEWAY_SERVICE 생성

---
+++ insite-gateway-service/src/test/java/com/thirty/insitegatewayservice/InsiteGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirty.insitegatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsiteGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
