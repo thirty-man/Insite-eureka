82a8d0e6be145b8dd5fa75ec8b6fdc5e108ffece
wns882@naver.com <wns882@naver.com>
2023-10-28 13:20:29 +0900

    :tada: init: member-service 생성

---
+++ insite-member-service/src/main/java/com/thirty/insitememberservice/InsiteMemberServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thirty.insitememberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsiteMemberServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsiteMemberServiceApplication.class, args);
+	}
+
+}
---
+++ insite-member-service/src/test/java/com/thirty/insitememberservice/InsiteMemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirty.insitememberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsiteMemberServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
