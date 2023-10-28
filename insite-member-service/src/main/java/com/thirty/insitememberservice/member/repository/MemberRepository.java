6379cb2cc69f0b934a7fcd41f4ef2d4666e8cec9
wns882@naver.com <wns882@naver.com>
2023-10-28 19:15:17 +0900

    :sparkles: feat: 사용자 검증 sql 추가

---
+++ insite-member-service/src/main/java/com/thirty/insitememberservice/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.thirty.insitememberservice.member.repository;
+
+
+import com.thirty.insitememberservice.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByKakaoIdAndGoodByeTimeIsNull(String kakaoId);
+}
