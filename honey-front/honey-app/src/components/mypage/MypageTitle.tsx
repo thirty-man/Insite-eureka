cb54708174c0f96e3be752327bc005fa776f58e6
YS <dbtjq1592000@gmail.com>
2023-10-30 17:13:59 +0900

    :bug: fix: 타이틀 오류 수정

--- honey-front/honey-app/src/components/mypage/MypageTitle.tsx
+++ honey-front/honey-app/src/components/mypage/MypageTitle.tsx
@@ -32,11 +32,11 @@ function MypageTitle() {
 
   useEffect(() => {
     // selectedRoom이 변경될 때 title 업데이트
-    if (selectedRoom !== undefined) {
-      setTitle(selectedRoom.roomTitle);
-    } else if (roomList.length > 0) {
-      setTitle(roomList[0].roomTitle);
-      setSelectedRoom(roomList[0]);
+    if (roomList) {
+      if (roomList.length > 0) {
+        setTitle(roomList[0].roomTitle);
+        setSelectedRoom(roomList[0]);
+      }
     } else {
       setTitle("방을 선택하세요");
     }
