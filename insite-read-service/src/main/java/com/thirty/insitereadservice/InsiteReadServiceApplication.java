93902f8651b1c053fb83b08e46ae22eb1680ee8c
박현철 <wns882@naver.com>
2023-10-31 13:41:23 +0900

    :tada: init: read서비스 초기 생성

---
+++ insite-read-service/src/main/java/com/thirty/insitereadservice/InsiteReadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thirty.insitereadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsiteReadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsiteReadServiceApplication.class, args);
+	}
+
+}
