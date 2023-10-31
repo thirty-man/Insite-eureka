63439c4d6185035cc4a17ba461778f61ae141254
김유섭 <dbtjq1592000@gmail.com>
2023-10-31 21:34:18 +0900

    [4주차/FE] 인사이트 초기설정 일부 머지

--- insite-app/src/reducer.js
+++ insite-app/src/reducer.js
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux";
 import persistReducer from "redux-persist/es/persistReducer";
+import storageSession from "redux-persist/lib/storage/session";
 
 const persistConfig = {
   key: "root",
   storage: storageSession,
+  whitelist: [],
+  blacklist: [],
 };
 
 const rootReducer = combineReducers({});
