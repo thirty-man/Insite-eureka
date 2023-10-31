662b4628bc56d9b7bbfa48c9990728be1c5ab0f0
정연수 <jos6151@naver.com>
2023-10-31 12:53:43 +0900

    :pencil2: fix: redis 비밀번호 수정

--- insite-member-service/src/main/java/com/thirty/insitememberservice/global/config/redis/RedisConfig.java
+++ insite-member-service/src/main/java/com/thirty/insitememberservice/global/config/redis/RedisConfig.java
@@ -26,7 +26,7 @@ public class RedisConfig {
 		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
 		redisStandaloneConfiguration.setHostName(redisHost);
 		redisStandaloneConfiguration.setPort(Integer.parseInt(redisPort));
-//		redisStandaloneConfiguration.setPassword(redisPassword);
+		redisStandaloneConfiguration.setPassword(redisPassword);
 		LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
 		return lettuceConnectionFactory;
 	}
