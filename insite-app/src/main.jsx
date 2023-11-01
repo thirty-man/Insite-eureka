0fd0a6c9cc926507f9212fe5bfa676dffa115ba1
JaewoongHur <jwhur7@naver.com>
2023-11-01 11:09:09 +0900

    :art: refactor: settings

--- insite-app/src/main.jsx
+++ insite-app/src/main.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
+import { RouterProvider } from "react-router-dom";
+
 // import "@config/axios.config";
 // import { Provider } from "react-redux";
 
 // export const persistor = persistStore(store);
 // import store from "./store";
 // import { persistStore } from "redux-persist";
+
 import router from "./router";
-import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <Provider store={store}>
