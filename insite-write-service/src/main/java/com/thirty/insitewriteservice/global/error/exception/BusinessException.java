471813f7d66ccea7ee518f3318f8978a8262cd6c
박현철 <wns882@naver.com>
2023-11-01 16:05:35 +0900

    :sparkles: feat: 예외 클래스 생성

---
+++ insite-write-service/src/main/java/com/thirty/insitewriteservice/global/error/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.thirty.insitewriteservice.global.error.exception;
+
+import com.thirty.insitewriteservice.global.error.ErrorCode;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+	private ErrorCode errorCode;
+
+	public BusinessException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
+		this.errorCode = errorCode;
+	}
+}
