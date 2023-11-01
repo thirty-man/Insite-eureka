4fdc40651ae5542e54b2829f9e93d62570d65e16
김동현 <dhkim960511@naver.com>
2023-11-01 15:11:48 +0900

    :zap: feat: add config files

---
+++ insite-realtime-read-service/src/main/java/com/thirty/insiterealtimereadservice/InsiteRealtimeReadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thirty.insiterealtimereadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsiteRealtimeReadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsiteRealtimeReadServiceApplication.class, args);
+	}
+
+}
