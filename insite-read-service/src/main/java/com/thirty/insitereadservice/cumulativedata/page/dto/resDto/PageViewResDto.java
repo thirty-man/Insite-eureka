13a0cc43fdc10dd202f31ea152c3dce4d571738e
정연수 <jos6151@naver.com>
2023-11-01 16:24:50 +0900

    :sparkles: feat: PageViewResDto 생성

---
+++ insite-read-service/src/main/java/com/thirty/insitereadservice/cumulativedata/page/dto/resDto/PageViewResDto.java
@@ -0,0 +1,15 @@
+package com.thirty.insitereadservice.cumulativedata.page.dto.resDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageViewResDto {
+    private int pageView;
+
+}
