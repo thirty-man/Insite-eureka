ff26cc9edacabba313e7ca1cb6e2690d0a200dec
정연수 <jos6151@naver.com>
2023-11-01 16:24:21 +0900

    :sparkles: feat: 누적 페이지 서비스 추가

--- insite-read-service/src/main/java/com/thirty/insitereadservice/cumulativedata/page/service/PageService.java
+++ insite-read-service/src/main/java/com/thirty/insitereadservice/cumulativedata/page/service/PageService.java
@@ -1,42 +1,10 @@
 package com.thirty.insitereadservice.cumulativedata.page.service;
 
-import com.influxdb.client.InfluxDBClient;
-import com.influxdb.query.FluxRecord;
-import com.influxdb.query.FluxTable;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-import java.util.List;
+import com.thirty.insitereadservice.cumulativedata.page.dto.reqDto.PageViewReqDto;
+import com.thirty.insitereadservice.cumulativedata.page.dto.resDto.PageViewResDto;
 
-@Service
-public class PageService {
-
-    @Resource
-    private InfluxDBClient influxDBClient;
-
-    @Value("${influxdb.org}")
-    private String org;
-
-    @Value("${influxdb.bucket}")
-    private String bucket;
-
-//    public void queryData() {
-//        String flux = "from(bucket: \"insite\")"
-//                + " |> range(start: -1h)"
-//                + " |> filter(fn: (r) => r._measurement == \"data\" and r.serviceToken == \"1234\")";
-//        List<FluxTable> tables = influxDBClient.getQueryApi().query(flux);
-//
-//        for (FluxTable fluxTable : tables) {
-//            List<FluxRecord> records = fluxTable.getRecords();
-//            for (FluxRecord record : records) {
-//                // 여기서 각 레코드의 필드와 태그 값을 가져올 수 있습니다.
-//                String cookieId = record.getValueByKey("cookieId").toString();
-//                String currentUrl = record.getValueByKey("currentUrl").toString();
-//                // 나머지 필드와 태그도 동일한 방식으로 처리 가능
-//            }
-//        }
-//    }
+public interface PageService {
+    PageViewResDto getPageView(PageViewReqDto pageViewReqDto);
 
 }
