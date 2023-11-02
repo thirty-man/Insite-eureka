fb7a9bfed045767830e5eb853fef4203ce10f0f4
허재웅 <jwhur7@naver.com>
2023-11-02 10:48:08 +0900

    Front/feat/router

--- insite-app/src/components/common/button/ImageButton.tsx
+++ insite-app/src/components/common/button/ImageButton.tsx
@@ -4,6 +4,7 @@ interface ImageButtonProps {
   width: string;
   height: string;
   onClick: () => void;
+  borderRadius: string;
 }
 
 interface ImageProps extends ImageButtonProps {
@@ -21,18 +22,33 @@ const ImageButtonStyle = styled.button<ImageButtonProps>`
   height: ${(props) => props.height};
   cursor: pointer;
   background: transparent;
+  border-radius: ${(props) => props.borderRadius};
   border: none;
   padding: 0;
+  overflow: hidden;
 `;
 
 const ImageStyle = styled.img<ImageProps2>`
   width: 100%;
   height: 100%;
+  border-radius: inherit;
 `;
 
-function ImageButton({ width, height, onClick, src, alt }: ImageProps) {
+function ImageButton({
+  width,
+  height,
+  onClick,
+  borderRadius,
+  src,
+  alt,
+}: ImageProps) {
   return (
-    <ImageButtonStyle width={width} height={height} onClick={() => onClick()}>
+    <ImageButtonStyle
+      width={width}
+      height={height}
+      borderRadius={borderRadius}
+      onClick={() => onClick()}
+    >
       <ImageStyle src={src} alt={alt} />
     </ImageButtonStyle>
   );
--- insite-app/src/router.tsx
+++ insite-app/src/router.tsx
@@ -1,5 +1,16 @@
 import App from "@App";
+import ActiveUserPage from "@pages/activeuser/ActiveUserPage";
+import ApiTestPage from "@pages/apitest/ApiTestPage";
+import ButtonManagementPage from "@pages/buttonmanagement/ButtonManagementPage";
+import NotFoundErrorPage from "@pages/error/NotFoundErrorPage";
+import GuidPage from "@pages/guide/GuidePage";
 import LoginPage from "@pages/login/LoginPage";
+import MainPage from "@pages/main/MainPage";
+import MySitePage from "@pages/mysite/MySitePage";
+import RealTimePage from "@pages/realtime/RealTimePage";
+import ServiceManagementPage from "@pages/servicemanagement/ServiceManagementPage";
+import TrackingPage from "@pages/tracking/TrackingPage";
+import UserPage from "@pages/user/UserPage";
 import { RouteObject, createBrowserRouter } from "react-router-dom";
 
 const routePath: RouteObject[] = [
@@ -10,8 +21,65 @@ const routePath: RouteObject[] = [
   },
   {
     id: "main-page",
+    path: "/main",
+    element: <MainPage />,
+  },
+  {
+    id: "my-site",
+    path: "/mysite",
+    element: <MySitePage />,
+  },
+  {
+    id: "app",
     path: "",
     element: <App />,
+    children: [
+      {
+        id: "real-time",
+        path: "",
+        element: <RealTimePage />,
+      },
+      {
+        id: "service-management",
+        path: "/manage",
+        element: <ServiceManagementPage />,
+      },
+      {
+        id: "tracking",
+        path: "/track",
+        element: <TrackingPage />,
+      },
+      {
+        id: "user",
+        path: "/user",
+        element: <UserPage />,
+      },
+      {
+        id: "active-user",
+        path: "/active",
+        element: <ActiveUserPage />,
+      },
+      {
+        id: "button-management",
+        path: "/button",
+        element: <ButtonManagementPage />,
+      },
+      {
+        id: "api-test",
+        path: "/api",
+        element: <ApiTestPage />,
+      },
+      {
+        id: "guide",
+        path: "/guide",
+        element: <GuidPage />,
+      },
+    ],
+  },
+  {
+    id: "error",
+    path: "/*",
+    element: <NotFoundErrorPage />,
   },
 ];
 
