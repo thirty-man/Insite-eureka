f4259b7cb24b3baa594feaed19bd186dacd4c4e5
김유섭 <dbtjq1592000@gmail.com>
2023-11-03 10:44:42 +0900

    [4주차/FE] Axios 일부 추가

--- insite-app/src/pages/main/MainPage.tsx
+++ insite-app/src/pages/main/MainPage.tsx
@@ -1,6 +1,21 @@
 import Header from "@components/common/header/Header";
+import { useEffect } from "react";
+import useAxios from "@api/useAxios";
 
 function MainPage() {
+  const initialConfig = {
+    method: "get",
+    url: "/health/check",
+  };
+
+  const { response, error, loading, fetchData } = useAxios(initialConfig);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  console.log(response?.data);
+
   return (
     <div>
       <Header />
