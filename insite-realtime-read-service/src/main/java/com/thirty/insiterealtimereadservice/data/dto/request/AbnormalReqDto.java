a534e031a0a36fc52e351f6c7e92cf2a849bae7d
김동현 <dhkim960511@naver.com>
2023-11-03 10:36:47 +0900

    :art: fix: post 변경으로인한 dto 생성

---
+++ insite-realtime-read-service/src/main/java/com/thirty/insiterealtimereadservice/data/dto/request/AbnormalReqDto.java
@@ -0,0 +1,14 @@
+package com.thirty.insiterealtimereadservice.data.dto.request;
+
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AbnormalReqDto {
+    @NotNull(message = "토큰을 기입해주세요")
+    private String token;
+}
--- insite-realtime-read-service/src/main/java/com/thirty/insiterealtimereadservice/feignclient/dto/request/MemberValidReqDto.java
+++ insite-realtime-read-service/src/main/java/com/thirty/insiterealtimereadservice/feignclient/dto/request/MemberValidReqDto.java
@@ -14,9 +14,13 @@ public class MemberValidReqDto {
     @NotNull(message = "토큰값을 기입해주세요")
     private String token;
 
-    public static MemberValidReqDto create(String token){
+    @NotNull(message = "멤버 아이디를 기입해주세요")
+    private int memberId;
+
+    public static MemberValidReqDto create(String token, int memberId){
         return MemberValidReqDto.builder()
             .token(token)
+            .memberId(memberId)
             .build();
     }
 }
