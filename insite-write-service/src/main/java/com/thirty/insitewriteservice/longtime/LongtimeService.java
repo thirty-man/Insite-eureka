7ef63b5b116b7fe98ffe38c4384bb688adcaf60f
박현철 <wns882@naver.com>
2023-11-06 13:33:01 +0900

    :zap: feat: 버튼 추가

--- insite-write-service/src/main/java/com/thirty/insitewriteservice/longtime/LongtimeService.java
+++ insite-write-service/src/main/java/com/thirty/insitewriteservice/longtime/LongtimeService.java
@@ -1,8 +1,11 @@
 package com.thirty.insitewriteservice.longtime;
 
+import com.thirty.insitewriteservice.write.dto.ButtonReqDto;
 import com.thirty.insitewriteservice.write.dto.DataReqDto;
 
 public interface LongtimeService {
 
 	void writeLongData(DataReqDto dataReqDto);
+
+	void writeLongButton(ButtonReqDto buttonReqDto);
 }
--- insite-write-service/src/main/java/com/thirty/insitewriteservice/longtime/LongtimeServiceImpl.java
+++ insite-write-service/src/main/java/com/thirty/insitewriteservice/longtime/LongtimeServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.thirty.insitewriteservice.global.service.InfluxDBService;
 import com.thirty.insitewriteservice.global.service.KafkaProducer;
+import com.thirty.insitewriteservice.write.dto.ButtonReqDto;
 import com.thirty.insitewriteservice.write.dto.DataReqDto;
 
 import lombok.RequiredArgsConstructor;
@@ -17,8 +18,13 @@ public class LongtimeServiceImpl implements LongtimeService {
 	@Override
 	public void writeLongData(DataReqDto dataReqDto) {
 
-		kafkaProducer.send("data", dataReqDto);
+		kafkaProducer.sendData("data", dataReqDto);
 
 		//influxDBService.writeDataToData(dataReqDto);
+	}
+
+	@Override
+	public void writeLongButton(ButtonReqDto buttonReqDto) {
+		kafkaProducer.sendButton("button", buttonReqDto);
 	}
 }
