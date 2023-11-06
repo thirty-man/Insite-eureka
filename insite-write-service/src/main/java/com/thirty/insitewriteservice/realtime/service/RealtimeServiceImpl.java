d3a91e8176ceb5f1e8c2a644a6078c7b7df8f7fc
박현철 <wns882@naver.com>
2023-11-06 11:20:04 +0900

    :fire: remove: 쓰기 기능 추가

--- insite-write-service/src/main/java/com/thirty/insitewriteservice/realtime/service/RealtimeServiceImpl.java
+++ insite-write-service/src/main/java/com/thirty/insitewriteservice/realtime/service/RealtimeServiceImpl.java
@@ -2,7 +2,6 @@ package com.thirty.insitewriteservice.realtime.service;
 
 import org.springframework.stereotype.Service;
 
-import com.thirty.insitewriteservice.realtime.repository.RealtimeDataRepository;
 import com.thirty.insitewriteservice.write.dto.DataReqDto;
 
 import lombok.RequiredArgsConstructor;
@@ -10,10 +9,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class RealtimeServiceImpl implements RealtimeService {
-	private final RealtimeDataRepository realtimeDataRepository;
 
 	@Override
 	public void writeRealData(DataReqDto dataReqDto) {
-		realtimeDataRepository.save(dataReqDto.toRealtimeDataOf());
 	}
 }
