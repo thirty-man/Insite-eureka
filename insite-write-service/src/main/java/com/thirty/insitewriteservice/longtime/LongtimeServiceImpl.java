d21df7c0e3bb4d86abb63dca47fa4fa61b53ea9f
박현철 <wns882@naver.com>
2023-11-06 11:19:16 +0900

    :zap: feat: 데이터 쓰기 기능 추가

--- insite-write-service/src/main/java/com/thirty/insitewriteservice/longtime/LongtimeServiceImpl.java
+++ insite-write-service/src/main/java/com/thirty/insitewriteservice/longtime/LongtimeServiceImpl.java
@@ -3,6 +3,7 @@ package com.thirty.insitewriteservice.longtime;
 import org.springframework.stereotype.Service;
 
 import com.thirty.insitewriteservice.global.service.InfluxDBService;
+import com.thirty.insitewriteservice.global.service.KafkaProducer;
 import com.thirty.insitewriteservice.write.dto.DataReqDto;
 
 import lombok.RequiredArgsConstructor;
@@ -11,9 +12,13 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class LongtimeServiceImpl implements LongtimeService {
 	private final InfluxDBService influxDBService;
+	private final KafkaProducer kafkaProducer;
 
 	@Override
 	public void writeLongData(DataReqDto dataReqDto) {
-		influxDBService.writeDataToData(dataReqDto);
+
+		kafkaProducer.send("data", dataReqDto);
+
+		//influxDBService.writeDataToData(dataReqDto);
 	}
 }
