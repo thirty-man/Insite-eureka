201c48c65ac6cf2185cdf1796c93977421646995
김동현 <dhkim960511@naver.com>
2023-11-06 14:30:32 +0900

    :zap: feat: add feignClient

---
+++ insite-read-service/src/main/java/com/thirty/insitereadservice/feignclient/MemberServiceClient.java
@@ -0,0 +1,16 @@
+package com.thirty.insitereadservice.feignclient;
+
+import com.thirty.insitereadservice.feignclient.dto.request.MemberValidReqDto;
+import javax.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "insite-member-service")
+public interface MemberServiceClient {
+
+    @PostMapping(value = "api/v1/members/valid")
+    void validationMemberAndApplication(
+        @Valid @RequestBody MemberValidReqDto memberValidReqDto
+    );
+}
