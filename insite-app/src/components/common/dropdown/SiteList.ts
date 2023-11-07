131b8e95b4a4bce8683a5c68f072a533c0c907b5
정연수 <jos6151@naver.com>
2023-11-07 17:22:32 +0900

    :zap: feat: MySite 페이지 구현중

--- insite-app/src/components/common/dropdown/SiteList.ts
+++ insite-app/src/components/common/dropdown/SiteList.ts
@@ -5,12 +5,6 @@ const SiteList: ItemTypes[] = [
   { id: 1, name: "naver.com" },
   { id: 2, name: "google.com" },
   { id: 3, name: "moduo.kr" },
-  { id: 4, name: "moduo.kr" },
-  { id: 5, name: "moduo.kr" },
-  { id: 6, name: "moduo.kr" },
-  { id: 7, name: "moduo.kr" },
-  { id: 8, name: "moduo.kr" },
-  { id: 9, name: "moduo.kr" },
 ];
 
 export default SiteList;
---
+++ insite-app/src/components/common/header/MainHeader.tsx
@@ -0,0 +1,165 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { RootState } from "@reducer";
+import { useDispatch, useSelector } from "react-redux";
+import { setOpenProfile } from "@reducer/HeaderModalStateInfo";
+import { myprofile } from "@assets/icons";
+import styled from "styled-components";
+import Modal from "../modal/Modal";
+import DropDown from "../dropdown/DropDown";
+import SiteList from "../dropdown/SiteList";
+import ImageButton from "../button/ImageButton";
+import { homeLogo } from "@assets/images";
+
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  height: 10%;
+  top: 0;
+  right: 0;
+  background-color: ${(props) => props.theme.colors.b2};
+`;
+
+// const HeaderWrapper = styled.div`
+//   width: 100%;
+//   height: 50%;
+//   margin-top: 15px;
+//   display: flex;
+//   flex-direction: row;
+//   align-items: center;
+//   justify-content: flex-end;
+//   font-weight: 900;
+// `;
+const HeaderWrapper = styled.div`
+  width: 100%;
+  height: 50%;
+  margin-top: 15px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: 900;
+`;
+
+const ProfileWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
+
+const ProfileImg = styled.img`
+  display: flex;
+  align-items: center;
+//   justify-content: center;
+  width: 3rem;
+  height: 3rem;
+  margin-top: 10px;
+  margin-right: 35px;
+  margin-left: 15px;
+  cursor: pointer;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+//   justify-content: center;
+  width: 100%;
+  height: 10%;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+const LogoImgWrapper = styled.div`
+  display: flex;
+  align-items: center;
+//   justify-content: center;
+`;
+
+
+function MainHeader(){
+    const navi = useNavigate();
+    const dispatch = useDispatch();
+    const location = useLocation();
+    const openDropdown = useSelector(
+      (state: RootState) => state.HeaderModalStateInfo.openDropdown,
+    );
+    const [isProfile, setIsProfile] = useState<boolean>(false);
+    const [currentPathname, setCurrentPathname] = useState<string>("");
+    useEffect(() => {
+      if (openDropdown) {
+        setIsProfile(false);
+        dispatch(setOpenProfile(false));
+      }
+    }, [openDropdown, dispatch, setIsProfile]);
+  
+    useEffect(() => {
+      setCurrentPathname(location.pathname);
+    }, [location]);
+  
+    const handleOpenProfile = (e: React.MouseEvent) => {
+      e.stopPropagation();
+      const newIsProfile = !isProfile;
+      setIsProfile(newIsProfile);
+      dispatch(setOpenProfile(newIsProfile));
+    };
+    return (
+        <HeaderContainer>
+            <HeaderWrapper>
+                <LogoContainer>
+                    <LogoImgWrapper>
+                    <ImageButton
+                        width="100%"
+                        height="100%"
+                        src={homeLogo}
+                        alt="insite Home Logo"
+                        onClick={() => navi("/main")}
+                    />
+                    </LogoImgWrapper>
+                </LogoContainer>
+        <ProfileWrapper>
+          <ProfileImg
+            src={myprofile}
+            alt="my profile"
+            onClick={handleOpenProfile}
+          />
+          {isProfile && (
+            <Modal
+              width="15rem"
+              height="6.5rem"
+              $posX="-50%"
+              $posY="80%"
+              close={() => setIsProfile(false)}
+              position="absolute"
+            >
+              {/* <Option
+                onClick={() => {
+                  navi("/login");
+                  setIsProfile(false);
+                }}
+              >
+                로그인 / 로그아웃
+              </Option> */}
+              {/* <Option
+                onClick={() => {
+                  if (currentPathname !== "/main") {
+                    navi("/main");
+                  } else {
+                    navi("/mysite");
+                  }
+
+                  setIsProfile(false);
+                }}
+              >
+                {currentPathname === "/main"
+                  ? "사이트 선택하러 가기"
+                  : "메인으로 가기"}
+              </Option> */}
+            </Modal>
+          )}
+        </ProfileWrapper>
+            </HeaderWrapper>
+        </HeaderContainer>
+    );
+}
+export default MainHeader;
