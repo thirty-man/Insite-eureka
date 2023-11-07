39e4377b36a21c6bfd6f8ba0e593b6df7094b3df
박현철 <wns882@naver.com>
2023-11-07 13:10:15 +0900

    :sparkles: feat: 사용자 버튼 클릭 추적을 위한 스크립트 코드 tsx , jsx 버전

---
+++ insite-script/Tracebutton.tsx
@@ -0,0 +1,71 @@
+type AnalyticsData = {
+  name: string;
+  current_url: string;
+  cookie_id: string;
+  application_token: string;
+  application_url: string;
+  activity_id: string;
+};
+
+// cookeId 를 위한 uniqueId 생성합니다.
+function generateUniqueId() {
+  return "id_" + Math.random().toString(36).substr(2, 16);
+}
+
+// 기존의 cookieId 를 가져옵니다.
+function getCookieId() {
+  const cookieId = document.cookie.replace(
+    /(?:(?:^|.*;\s*)analyticsId\s*\=\s*([^;]*).*$)|^.*$/,
+    "$1",
+  );
+  return cookieId;
+}
+
+// 새로운 cookieId 생성을 위한 함수입니다.
+function createCookieId() {
+  const cookieId = generateUniqueId();
+  document.cookie = `analyticsId=${cookieId};path=/;expires=${new Date(
+    new Date().setFullYear(new Date().getFullYear() + 1),
+  ).toUTCString()}`;
+  return cookieId;
+}
+
+// Document의 정보를 집계하는 함수입니다.
+export function traceButton(name : string) {
+  const path = window.location.pathname;
+  const cookieId = getCookieId() || createCookieId();
+  const data = {
+    name : name,
+    current_url: path,
+    cookie_id: cookieId,
+    application_token: "test",
+    application_url: window.location.origin,
+    activity_id: "test",
+  };
+
+  sendData(data);
+}
+
+// 집계된 데이터를 전송하는 함수입니다.
+function sendData(data: AnalyticsData) {
+  console.log(data);
+  fetch("http://localhost:8082/api/v1/data/button", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error,
+      );
+    });
+}
