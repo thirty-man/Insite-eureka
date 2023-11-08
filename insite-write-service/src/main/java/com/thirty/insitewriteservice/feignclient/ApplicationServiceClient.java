8f031e60bc8794da4292c34cd6329e43d7d9f7da
박현철 <wns882@naver.com>
2023-11-08 17:31:26 +0900

    :zap: feat: feignclient 주소 변경

--- insite-write-service/src/main/java/com/thirty/insitewriteservice/feignclient/ApplicationServiceClient.java
+++ insite-write-service/src/main/java/com/thirty/insitewriteservice/feignclient/ApplicationServiceClient.java
@@ -1,16 +1,17 @@
 package com.thirty.insitewriteservice.feignclient;
 
-import com.thirty.insitewriteservice.feignclient.dto.request.ApplicationVerifyReqDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.cloud.openfeign.FeignClient;
-
 import javax.validation.Valid;
 
-@FeignClient(name = "applicationServiceClient", url = "http://localhost:8081")
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.thirty.insitewriteservice.feignclient.dto.request.ApplicationVerifyReqDto;
+
+@FeignClient(name = "insite-member-service")
 public interface ApplicationServiceClient {
 
-    @PostMapping(value = "/api/v1/application/verify")
-    void validationApplication(@Valid @RequestBody ApplicationVerifyReqDto applicationVerifyReqDto);
+	@PostMapping(value = "/api/v1/application/verify")
+	void validationApplication(@Valid @RequestBody ApplicationVerifyReqDto applicationVerifyReqDto);
 }
 
