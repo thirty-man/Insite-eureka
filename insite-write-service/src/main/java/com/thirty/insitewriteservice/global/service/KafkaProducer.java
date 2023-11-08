7cb2934e6665c3e9014c801880b6c987794d9245
김성규 <tjdrb9999@naver.com>
2023-11-08 09:36:13 +0900

    ⚡ realtime 기능 제거

--- insite-write-service/src/main/java/com/thirty/insitewriteservice/global/service/KafkaProducer.java
+++ insite-write-service/src/main/java/com/thirty/insitewriteservice/global/service/KafkaProducer.java
@@ -100,5 +100,4 @@ public class KafkaProducer {
 
 		return sb.toString();
 	}
-
 }
--- insite-write-service/src/main/java/com/thirty/insitewriteservice/realtime/service/RealtimeService.java
+++
@@ -1,7 +0,0 @@
-//package com.thirty.insitewriteservice.realtime.service;
-//
-//import com.thirty.insitewriteservice.write.dto.DataReqDto;
-//
-//public interface RealtimeService {
-//	void writeRealData(DataReqDto dataReqDto);
-//}
