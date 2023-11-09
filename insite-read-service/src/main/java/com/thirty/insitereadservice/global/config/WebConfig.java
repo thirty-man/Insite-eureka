782f9bc01543c4b7ff6179ee2b0fee46eadbb678
YS <dbtjq1592000@gmail.com>
2023-11-10 08:11:35 +0900

    :bug: fix: webconfig수정

--- insite-read-service/src/main/java/com/thirty/insitereadservice/global/config/WebConfig.java
+++ insite-read-service/src/main/java/com/thirty/insitereadservice/global/config/WebConfig.java
@@ -9,7 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedHeaders("Authorization", "RefreshToken")
+                .allowedHeaders("*")
+                .exposedHeaders("RefreshToken")
+                .exposedHeaders("Authorization")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedOrigins("*");
     }
