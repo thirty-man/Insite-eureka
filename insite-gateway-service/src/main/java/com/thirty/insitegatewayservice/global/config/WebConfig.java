1d56175884968addbfce7df3f1da33fbdb4c4cef
박현철 <wns882@naver.com>
2023-11-13 11:06:42 +0900

    :zap: feat: cors config추가

---
+++ insite-gateway-service/src/main/java/com/thirty/insitegatewayservice/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.thirty.insitegatewayservice.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedHeaders("*")
+			.exposedHeaders("RefreshToken")
+			.exposedHeaders("Authorization")
+			.allowedMethods("GET", "POST", "PUT", "DELETE")
+			.allowedOrigins("*");
+	}
+}
+
