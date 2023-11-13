870cdaa3128825ef858c3f6cd0e65f8731605b58
김동현 <dhkim960511@naver.com>
2023-11-13 17:45:32 +0900

    :art: refactor: JwtProcess 리팩토링

--- insite-read-service/src/main/java/com/thirty/insitereadservice/global/jwt/JwtProcess.java
+++ insite-read-service/src/main/java/com/thirty/insitereadservice/global/jwt/JwtProcess.java
@@ -8,14 +8,17 @@ import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.thirty.insitereadservice.global.error.ErrorCode;
 import com.thirty.insitereadservice.global.error.exception.TokenException;
+import javax.servlet.http.HttpServletRequest;
 
 public class JwtProcess {
 
 	// 액세스 토큰 검증 (return 되는 LoginUser 객체를 강제로 시큐리티 세션에 직접 주입할 예정)
-	public static int verifyAccessToken(String token) {
+	public static int verifyAccessToken(HttpServletRequest request) {
+
+		String jwtToken = request.getHeader(JwtVO.HEADER).replace(JwtVO.TOKEN_PREFIX, "");
 		//받아온 토큰을 디코드 하고 검증하기
 		try {
-			DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(JwtVO.SECRET)).build().verify(token);
+			DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(JwtVO.SECRET)).build().verify(jwtToken);
 			int memberId = decodedJWT.getClaim("memberId").asInt(); //토큰 내부는 암호화 안되어 있으니까 최소한의 정보만 넣자
 
 			return memberId;
