9ace491dadf63e7f95992f39c6b71321a9881f79
YS <dbtjq1592000@gmail.com>
2023-11-14 07:39:17 +0900

    :bug: fix: 폰트 포맷 변경

--- insite-app/src/assets/styles/globalStyles.ts
+++ insite-app/src/assets/styles/globalStyles.ts
@@ -7,62 +7,62 @@ const GlobalStyles = createGlobalStyle`
   @font-face {
     font-family: "Pretendard";
     font-weight: 100;
-    src: url("src/assets/fonts/Pretendard-Thin.woff") format("woff");
+    src: url("src/assets/fonts/Pretendard-Thin.woff") format("font-woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 200;
-    src: url("src/assets/fonts/Pretendard-ExtraLight.woff") format("woff");
+    src: url("src/assets/fonts/Pretendard-ExtraLight.woff") format("font-woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 300;
-    src: url("src/assets/fonts/Pretendard-Light.woff") format("woff");
+    src: url("src/assets/fonts/Pretendard-Light.woff") format("font-woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 400;
     font-style: normal;
-    src: url("src/assets/fonts/Pretendard-Regular.woff") format("woff");
+    src: url("src/assets/fonts/Pretendard-Regular.woff") format("font-woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 500;
-    src: url("src/assets/fonts/Pretendard-Medium.woff") format("woff");
+    src: url("src/assets/fonts/Pretendard-Medium.woff") format("font-woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 600;
-    src: url("src/assets/fonts/Pretendard-SemiBold.woff") format("woff");
+    src: url("src/assets/fonts/Pretendard-SemiBold.woff") format("font-woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 700;
     font-style: bold;
-    src: url("src/assets/fonts/Pretendard-Bold.woff") format("woff");
+    src: url("src/assets/fonts/Pretendard-Bold.woff") format("font-woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 800;
-    src: url("src/assets/fonts/Pretendard-ExtraBold.woff") format("woff");
+    src: url("src/assets/fonts/Pretendard-ExtraBold.woff") format("font-woff");
   }
 
   @font-face {
     font-family: "Pretendard";
     font-weight: 900;
-    src: url("src/assets/fonts/Pretendard-Black.woff") format("woff");
+    src: url("src/assets/fonts/Pretendard-Black.woff") format("font-woff");
   }
 
   @font-face {
     font-family: "Cafe24 Ssurround Air";
-    src: url("src/assets/fonts/Cafe24-Ssurround-Air.woff") format("woff");
+    src: url("src/assets/fonts/Cafe24-Ssurround-Air.woff") format("font-woff");
   }
   * {
     font-family: "Pretendard", sans-serif;
