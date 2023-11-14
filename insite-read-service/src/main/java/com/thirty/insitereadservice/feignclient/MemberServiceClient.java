1ee534acdec68fde873140491d189c3632799463
김동현 <dhkim960511@naver.com>
2023-11-14 11:48:40 +0900

    :pencil2: fix: feign client url 수정

--- insite-read-service/src/main/java/com/thirty/insitereadservice/feignclient/MemberServiceClient.java
+++ insite-read-service/src/main/java/com/thirty/insitereadservice/feignclient/MemberServiceClient.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-//eureka 안 키면 url로 로컬 서버를 등록해줘야 통신 가능
-//eureka 키면 name으로 통신 가능해서 지워줄 것
-@FeignClient(name = "insite-member-service",url = "http://localhost:8081")
+@FeignClient(name = "insite-member-service")
 public interface MemberServiceClient {
 
     @PostMapping(value = "/api/v1/members/valid")
