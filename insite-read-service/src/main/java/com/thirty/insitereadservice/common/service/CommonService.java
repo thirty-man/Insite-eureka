0a457504ea53aeb8c71e5e45eb937c7c85b2f887
정연수 <jos6151@naver.com>
2023-11-14 11:12:26 +0900

    Revert "Merge branch 'back/feat/read-service' into 'insite-member-dev'"

--- insite-read-service/src/main/java/com/thirty/insitereadservice/common/service/CommonService.java
+++
@@ -1,9 +0,0 @@
-package com.thirty.insitereadservice.common.service;
-
-import com.thirty.insitereadservice.common.dto.request.CommonReqDto;
-import com.thirty.insitereadservice.common.dto.response.CommonResDto;
-
-public interface CommonService {
-
-    CommonResDto getCommonInfo(CommonReqDto commonReqDto, int memberId);
-}
