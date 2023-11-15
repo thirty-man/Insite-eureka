aa5455ce8f3f0d8bc0039eeef3c636e156c3e831
YS <dbtjq1592000@gmail.com>
2023-11-15 16:45:18 +0900

    버튼통계

--- insite-app/src/components/realtime/ButtonStatistics.tsx
+++ insite-app/src/components/realtime/ButtonStatistics.tsx
@@ -44,7 +44,7 @@ function ButtonStatistics() {
             <TableRow key={item.id}>
               <TableCell>{item.name}</TableCell>
               <TableCell>{item.count}</TableCell>
-              <TableCell>{item.countPerUser}</TableCell>
+              <TableCell>{(+item.countPerUser * 100).toFixed(2)}%</TableCell>
             </TableRow>
           ))}
         </TableBody>
