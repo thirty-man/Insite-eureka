9e0b7ecd9bcde5633d62e3922479fded0e2b7c9e
김성규 <tjdrb9999@naver.com>
2023-11-16 10:25:23 +0900

    ⚡ feat: InsiteTrackers.js 파일 생성

---
+++ insite-write-service/src/main/resources/static/ButtonTrackers.js
@@ -0,0 +1,64 @@
+(function() {
+    // script tag 에서 토큰을 가져옵니다.
+    function getTokenFromScriptTag() {
+        var scripts = document.getElementsByTagName('script');
+        for (var i = 0; i < scripts.length; i++) {
+            if (scripts[i].src.includes("InsiteTrackers.js")) {
+                return scripts[i].getAttribute('token') || '';
+            }
+        }
+        return '';
+    }
+
+    // cookeId 를 위한 uniqueId 생성합니다.
+    function generateUniqueId() {
+        return "id_" + Math.random().toString(36).substr(2, 16);
+    }
+
+    // 기존의 cookieId 를 가져옵니다.
+    function getCookieId() {
+        var cookieId = document.cookie.replace(
+            /(?:(?:^|.*;\s*)analyticsId\s*\=\s*([^;]*).*$)|^.*$/,
+            "$1",
+        );
+        return cookieId;
+    }
+
+    // 새로운 cookieId 생성을 위한 함수입니다.
+    function createCookieId() {
+        var cookieId = generateUniqueId();
+        document.cookie = 'analyticsId=' + cookieId + ';path=/;expires=' + new Date(
+                        new Date().setFullYear(new Date().getFullYear() + 1)
+                    ).toUTCString();
+        return cookieId;
+    }
+
+    // Data를 전송하는 함수입니다.
+    function sendData(data) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://takeinsite.com:8081/write-service/api/v1/data/button', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status !== 200) {
+                console.error('There has been a problem with your fetch operation:', xhr.statusText);
+            }
+        };
+        xhr.send(JSON.stringify(data));
+    }
+
+    window.addEventListener('load', function() {
+        var path = window.location.pathname;
+        var currentCookieId = getCookieId() || createCookieId();
+        var data = {
+            cookie_id: currentCookieId,
+            current_url: path,
+            name: buttonName,
+            application_token: getTokenFromScriptTag(),
+            application_url: window.location.origin,
+            activity_id: "test",
+        };
+        sendData(data);
+    });
+
+
+})();
