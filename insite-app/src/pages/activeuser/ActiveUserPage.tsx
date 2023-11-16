72170395d5dc84392c83351d9fac1436e35b2975
JaewoongHur <jwhur7@naver.com>
2023-11-16 17:29:12 +0900

    fix

--- insite-app/src/pages/activeuser/ActiveUserPage.tsx
+++ insite-app/src/pages/activeuser/ActiveUserPage.tsx
@@ -48,7 +48,14 @@ function ActiveUserPage() {
     <>
       <FirstCol>
         <DefaultBox width="30rem" height="25rem">
-          <TitleBox text="텍스트">활동 사용자 수 조회</TitleBox>
+          <TitleBox
+            text="기간 내 활동 사용자 수 조회 정보를 제공합니다.
+각 페이지 별 활동 사용자 수를 내림차순으로 제공함으로써
+활동 사용자들이 어떤 페이지를 많이 사용하는지 파악할 수 있습니다.
+"
+          >
+            활동 사용자 수 조회
+          </TitleBox>
           <ContentDiv>
             <TextBox width="90%" height="90%">
               <ActiveUserStatistics />
@@ -56,7 +63,14 @@ function ActiveUserPage() {
           </ContentDiv>
         </DefaultBox>
         <DefaultBox width="30rem" height="25rem">
-          <TitleBox text="텍스트">평균 체류 시간</TitleBox>
+          <TitleBox
+            text="기간 내 활동 사용자 별 평균 체류 시간 정보를 제공합니다.
+활동 사용자들이 어떤 페이지에서 오래 머무르는지 파악해.
+서비스 개선에 적용할 수 있습니다.
+"
+          >
+            평균 체류 시간
+          </TitleBox>
           <ContentDiv>
             <TextBox width="90%" height="90%">
               <AverageActiveTimeByUrl />
@@ -64,7 +78,14 @@ function ActiveUserPage() {
           </ContentDiv>
         </DefaultBox>
         <DefaultBox width="30rem" height="25rem">
-          <TitleBox text="텍스트">사용자당 활동 횟수</TitleBox>
+          <TitleBox
+            text="기간 내 사용자 별 페이지에 따른 평균 활동 횟수를 제공합니다.
+어떤 페이지에 사용자가 자주 방문하는지 파악하여
+서비스 개선에 적용할 수 있습니다.
+"
+          >
+            사용자당 활동 횟수
+          </TitleBox>
           <ContentDiv>
             <TextBox width="90%" height="90%">
               <ActiveUserPerUserbyUrl />
@@ -74,7 +95,14 @@ function ActiveUserPage() {
       </FirstCol>
       <SecondCol>
         <DefaultBox width="30rem" height="25rem">
-          <TitleBox text="텍스트">총 사용자 수</TitleBox>
+          <TitleBox
+            text="기간 내 전체 사용자 수와 활동 사용자 수를 제공합니다.
+Cookie Id기반의 사용자 수와 활동 시간에 따라 부여된 Activity Id로 나뉜
+활동 사용자 수를 파악할 수 있습니다. 
+"
+          >
+            총 사용자 수
+          </TitleBox>
           <ContentDiv>
             <TextBox width="90%" height="90%">
               <TotalUser />
@@ -82,7 +110,13 @@ function ActiveUserPage() {
           </ContentDiv>
         </DefaultBox>
         <DefaultBox width="30rem" height="25rem">
-          <TitleBox text="텍스트">OS 별 활동 사용자 수</TitleBox>
+          <TitleBox
+            text="기간 내 어떤 운영체제를 통해 해당 서비스에 접근했는지 파악할 수 있습니다.
+서비스를 제공하는 플랫폼 확장 계획에 적용할 수 있습니다.
+"
+          >
+            OS 별 활동 사용자 수
+          </TitleBox>
           <ContentDiv>
             <TextBox width="90%" height="90%">
               <OsActiveUser />
@@ -90,7 +124,14 @@ function ActiveUserPage() {
           </ContentDiv>
         </DefaultBox>
         <DefaultBox width="30rem" height="25rem">
-          <TitleBox text="텍스트">활동당 페이지 조회수</TitleBox>
+          <TitleBox
+            text="기간 내 페이지 별 전체 활동 횟수와 활동 사용자 별 평균 활동 횟수를 제공합니다.
+활동 사용자가 자주 사용하는 페이지를 파악할 수 있어
+서비스 개선에 적용할 수 있습니다.
+"
+          >
+            활동당 페이지 조회수
+          </TitleBox>
           <ContentDiv>
             <TextBox width="90%" height="90%">
               <PageUsagePerActiveUser />
@@ -100,7 +141,14 @@ function ActiveUserPage() {
       </SecondCol>
       <ThirdCol>
         <DefaultBox width="102rem" height="25rem">
-          <TitleBox text="텍스트">시간 별 활동 사용자 수</TitleBox>
+          <TitleBox
+            text="기간 내 시간대 별 활동 사용자 수를 제공합니다.
+시간대에 따라 서비스를 이용하는 활동 사용자 수를 파악할 수 있어
+패치 시간 설정 등에 적용할 수 있습니다.
+"
+          >
+            시간 별 활동 사용자 수
+          </TitleBox>
           <ContentDiv>
             <ActiveUserPerTime />
           </ContentDiv>
--- insite-app/src/pages/buttonmanagement/ButtonManagementPage.tsx
+++ insite-app/src/pages/buttonmanagement/ButtonManagementPage.tsx
@@ -71,19 +71,38 @@ function ButtonManagementPage() {
     <>
       <FirstCol>
         <DefaultBox width="30rem" height="25rem">
-          <TitleBox text="텍스트">버튼 통계</TitleBox>
+          <TitleBox
+            text="기간 내 버튼 이용 통계를 제공합니다.
+어떤 버튼이 자주 활용되고, 어떤 이벤트가 자주 발생하는지 파악할 수 있습니다.
+서비스 개선에 적용할 수 있습니다.
+"
+          >
+            버튼 통계
+          </TitleBox>
           <ContentDiv>
             <ButtonStatistics />
           </ContentDiv>
         </DefaultBox>
         <DefaultBox width="62rem" height="25rem">
-          <TitleBox text="텍스트">버튼 클릭 로그</TitleBox>
+          <TitleBox
+            text="기간 내 버튼 클릭 로그와 사용자당 버튼 클릭 평균 횟수를 확인할 수 있습니다.
+버튼 이벤트 개선에 적용할 수 있습니다.
+"
+          >
+            버튼 클릭 로그
+          </TitleBox>
           <ButtonClickLogs />
         </DefaultBox>
       </FirstCol>
       <SecondCol>
         <DefaultBox width="102rem" height="25rem">
-          <TitleBox text="텍스트">버튼 누른 횟수</TitleBox>
+          <TitleBox
+            text="기간 내 전체 이용자가 버튼을 클릭한 횟수를 그래프로 파악할 수 있습니다.
+버튼 클릭 추이를 확인하여 버튼 이벤트 개선에 적용할 수 있습니다.
+"
+          >
+            버튼 누른 횟수
+          </TitleBox>
           <DropDown<ButtonType>
             items={buttonList}
             width="15rem"
