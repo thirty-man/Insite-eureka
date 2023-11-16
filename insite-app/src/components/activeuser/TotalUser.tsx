25582b379e337c465cde12309fee307e595eb836
Yuseop <dbtjq1592000@gmail.com>
2023-11-16 10:29:55 +0900

    test

--- insite-app/src/components/activeuser/TotalUser.tsx
+++ insite-app/src/components/activeuser/TotalUser.tsx
@@ -28,7 +28,7 @@ const Myeong = styled.p`
 
 function TotalUser() {
   const [activeData, setActiveData] = useState<number>(-1);
-  const [normalData, setNormalData] = useState<number>(-1);
+  const [, setNormalData] = useState<number>(-1);
   const startDateTime = useSelector(
     (state: RootState) => state.DateSelectionInfo.start,
   );
@@ -73,13 +73,13 @@ function TotalUser() {
     <OutDiv>
       <div>사용자</div>
       <CountBox>
-        {/* {normalData} */}
+        {/* {normalData.toLocaleString()} */}
         123123123123123123
         <Myeong>명</Myeong>
       </CountBox>
       <div>활동 사용자</div>
       <CountBox>
-        {activeData}
+        {activeData.toLocaleString()}
         <Myeong>명</Myeong>
       </CountBox>
     </OutDiv>
